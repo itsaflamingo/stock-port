911985f123dfaf5e6aa1a413a580a4b7f1202818
elizabeth pinero <elizabeth.pinero.r@gmail.com>
2025-10-26 12:43:20 +0000

    login and register page styling fixed

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -2,7 +2,7 @@ import ThemeButton from "@/components/theme-button"
 import Link from "next/link"
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className="w-full h-full flex flex-col justify-start align-center">
             <menu className="flex justify-end gap-2 p-2 h-15 mb-10">
                 <ThemeButton />
                 <div className="w-px h-8 bg-[var(--grey-border)] self-center" />
@@ -11,7 +11,10 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
                 </Link>
             </menu>
 
-            <main>{children}</main>
+            <main className="w-md h-5/10 flex flex-col items-center justify-center self-center
+        shadow-[0_12px_20px_5px_rgba(1,1,1,0.06)]
+        dark:shadow-[0_12px_20px_5px_rgba(255,255,255,0.3)]
+        gap-10">{children}</main>
 
             <footer>{/* optional footer here */}</footer>
         </div>
--- src/components/form-container.tsx
+++ src/components/form-container.tsx
@@ -7,12 +7,9 @@ type FormContainerProps = {
 
 export default function FormContainer({ form, ...props }: FormContainerProps) {
     return (
-        <div className="w-md h-4/10 flex flex-col items-center justify-center
-        shadow-[0_12px_20px_5px_rgba(1,1,1,0.06)]
-        dark:shadow-[0_12px_20px_5px_rgba(255,255,255,0.3)]
-        gap-10">
+        <>
             <h1 className="text-4xl font-bold text-black dark:text-white">{props.head}</h1>
             {form}
-        </div>
+        </>
     )
 }
